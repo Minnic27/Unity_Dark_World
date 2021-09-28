13a9639337f9c345f1c3c3b213b5603800b0aec9
Minnic27 <61070794+Minnic27@users.noreply.github.com>
2021-09-28 18:54:24 +0800

    Player Movement

---
+++ Dark World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPun
+{
+    CharacterController playerController;
+
+    public float speed = 2.0f;
+    public float accelaration = 8.0f;
+    public float gravity = -9.0f;
+    Vector3 velocity;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.IsMine) {
+
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            velocity = transform.TransformDirection(velocity);
+
+            // diagonal movement check
+
+            // if(Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.A))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+            // else if(Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+            // else if(Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.S))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+            // else if(Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.A))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+            // else if(Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.D))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+            // else if(Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.D))
+            // {
+            //     velocity *= 0f;
+            //     anim.SetFloat("Movement", 0.5f, 0.1f, Time.deltaTime); 
+            //     anim.SetInteger("Strafe", 0);
+            // }
+
+
+            // sprinting
+
+            if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))) 
+            {
+                anim.SetFloat("Movement", 0f, 0.1f, Time.deltaTime);
+                velocity *= accelaration;
+            }
+
+
+            // WASD movement with animatons
+
+            else if (Input.GetKey(KeyCode.W)) 
+            {
+                anim.SetFloat("Movement", 0.3f, 0.1f, Time.deltaTime);
+                velocity *= speed;
+            }
+
+            else if (Input.GetKey(KeyCode.A)) 
+            {
+                anim.SetInteger("Strafe", -1);
+                velocity *= speed;
+            }
+
+            else if (Input.GetKey(KeyCode.S)) 
+            {
+                anim.SetFloat("Movement", 1f, 0.1f, Time.deltaTime);
+                velocity *= speed;
+            }
+
+            else if (Input.GetKey(KeyCode.D)) 
+            {
+                anim.SetInteger("Strafe", 1);
+                velocity *= speed;
+            }
+
+            else // to idle animation
+            {
+                anim.SetFloat("Movement", 0.67f, 0.1f, Time.deltaTime); 
+                anim.SetInteger("Strafe", 0);
+            }
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity * Time.deltaTime);
+    }
+}
