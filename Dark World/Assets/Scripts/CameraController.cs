9df311b137a90d23b291f4d9eb970bf1367078ec
Minnic27 <61070794+Minnic27@users.noreply.github.com>
2021-09-29 21:45:10 +0800

    added CameraController

---
+++ Dark World/Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float rotationSpeed = 1;
+    public Transform target, player;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CamMovement();
+    }
+
+    void CamMovement()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Freeview");
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else 
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        
+    }
+}
