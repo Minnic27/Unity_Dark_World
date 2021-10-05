3c2bf432822e0580881d4c0d6e25e1e940adf497
Minnic27 <61070794+Minnic27@users.noreply.github.com>
2021-10-05 18:52:10 +0800

    changed enemy prefab

--- Dark World/Assets/Scripts/EnemyBehavior.cs
+++ Dark World/Assets/Scripts/EnemyBehavior.cs
@@ -8,8 +8,8 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField]
     private int health = 5;
 
-    public NavMeshAgent enemy;
-    public Transform player;
+    // public NavMeshAgent enemy;
+    // public Transform player;
 
 
     public void TakeDamage(int damageAmount)
@@ -28,13 +28,13 @@ public class EnemyBehavior : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void FollowPlayer()
-    {
-        enemy.SetDestination(player.position);
-    }
+    // private void FollowPlayer()
+    // {
+    //     enemy.SetDestination(player.position);
+    // }
 
-    void Update()
-    {
-        FollowPlayer();
-    }
+    // void Update()
+    // {
+    //     FollowPlayer();
+    // }
 }
